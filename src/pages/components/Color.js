fe66860aaf84c1325aaf92682e18f4fd071c6a66
Sarab Dar <darsarab421@gmail.com>
2024-06-01 22:02:03 +0500

    going to work on big image

--- src/pages/components/Color.js
+++ src/pages/components/Color.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { useNavigate } from "react-router-dom"; 
 import { useLocation } from 'react-router-dom';
 import greenColorCircle from "../../images/coloredCircleImages/greenColorCircle.jpg"
@@ -6,6 +6,7 @@ import pinkColorCircle from "../../images/coloredCircleImages/pinkColorCircle.jp
 import redColorCircle from "../../images/coloredCircleImages/redColorCircle.jpg";
 import skyColorCircle from "../../images/coloredCircleImages/skyColorCircle.jpg";
 import whiteColorCircle from "../../images/coloredCircleImages/whiteColorCircle.jpg";
+import "./Color.css"
 const Color = () => {
   
      const location = useLocation();
@@ -13,25 +14,76 @@ const Color = () => {
      const { name } = location.state || {};
 
        const [showBigImage, setShowBigImage] = useState(flavourPicked);
+      const [images, setImages] = useState({});
+
+const loadImages = async () => {
+  if (name === "small_round_cake_base") {
+    const [
+      roundChocolateFlavour,
+      roundChocolateInsidePink,
+      roundCream,
+      roundInsideFruit,
+      roundPistaGreen,
+      roundStawberry,
+    ] = await Promise.all([
+      import("../../images/RoundFlavour/roundChocolateFlavour.jpg"),
+      import("../../images/RoundFlavour/roundChocolateInsidePink.jpg"),
+      import("../../images/RoundFlavour/roundCream.jpg"),
+      import("../../images/RoundFlavour/roundInsideFruit.jpg"),
+      import("../../images/RoundFlavour/roundPistaGreen.jpg"),
+      import("../../images/RoundFlavour/roundStawberry.jpg"),
+    ]);
+    setImages({
+      image1: roundChocolateFlavour.default,
+      image2: roundChocolateInsidePink.default,
+      image3: roundCream.default,
+      image4: roundInsideFruit.default,
+      image5: roundPistaGreen.default,
+      image6: roundStawberry.default,
+    });
+  }
+  //  else if (number === 2) {
+  //   const []
+  // }
+};
+
+
+
+
+
+         useEffect(() => {
+           loadImages();
+         }, []);
   return (
-    <div>
+    <div className="color_file_main_container">
       <div className="top_container">
         <div className="showImgBig">
           <img src={showBigImage} alt="" />
         </div>
       </div>
       <div>
-        <div className="left_bar"></div>
-        <div className="base_shapes_container">
-         <div><img src={whiteColorCircle} alt="" /></div>
-         <div><img src={skyColorCircle} alt="" /></div>
-         <div><img src={pinkColorCircle} alt="" /></div>
-         <div><img src={redColorCircle} alt="" /></div>
-         <div><img src={greenColorCircle} alt="" /></div>
+        <div className="left_bar_in_color"></div>
+      
+        <div className="base_shapes_container ">
+          <div className="colorImagesDiv color_item1">
+            <img src={whiteColorCircle} alt="" />
+          </div>
+          <div className="colorImagesDiv color_item2">
+            <img src={skyColorCircle} alt="" />
+          </div>
+          <div className="colorImagesDiv color_item3">
+            <img src={pinkColorCircle} alt="" />
+          </div>
+          <div className="colorImagesDiv color_item4">
+            <img src={redColorCircle} alt="" />
+          </div>
+          <div className="colorImagesDiv color_item5">
+            <img src={greenColorCircle} alt="" />
+          </div>
         </div>
       </div>
       <div>
-        <button >Next</button>
+        <button>Next</button>
       </div>
     </div>
   );
